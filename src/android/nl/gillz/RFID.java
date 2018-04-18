ddc69481a61647bf0892929689b4effc5aeef20e
Lars Noorlander <lars@gillz.nl>
2018-04-18 14:18:21 +0200

    Included library

--- src/android/nl/gillz/RFID.java
+++ src/android/nl/gillz/RFID.java
@@ -8,21 +8,22 @@ import org.json.JSONException;
 public class RFID extends CordovaPlugin {
 
 	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
+	}
+
+	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if (action.equals("sum")) {
-			Integer num1 = args.getInt(0);
-			Integer num2 = args.getInt(1);
-			this.sum(num1, num2, callbackContext);
+		if (action.equals("scan")) {
+			// Integer num1 = args.getInt(0);
+			this.scan(callbackContext);
 			return true;
 		}
 		return false;
 	}
 
-	private void sum(Integer num1, Integer num2, CallbackContext callbackContext) {
-		if (num1 != null && num2 != null) {
-			callbackContext.success(num1 + num2);
-		} else {
-			callbackContext.error("Expected two integer arguments.");
-		}
+	private void scan(CallbackContext callbackContext) {
+		// callbackContext.error("Error");
+		callbackContext.success("Scanned");
 	}
 }
--- www/RFID.js
+++ www/RFID.js
@@ -1,3 +1,3 @@
-window.sum = function (num1, num2, successCallback, errorCallback) {
-	cordova.exec(successCallback, errorCallback, 'RFID', 'sum', [num1, num2]);
+window.scan = function (successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, 'RFID', 'scan', []);
 };
