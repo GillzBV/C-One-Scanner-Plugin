36b63b0e4d47b368b03083b41cd6cf55557a01f0
Lars Noorlander <lars@gillz.nl>
2018-04-25 17:43:48 +0200

    Added scanner

--- src/android/nl/gillz/RFID.java
+++ src/android/nl/gillz/RFID.java
@@ -1,6 +1,10 @@
 package nl.gillz;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import fr.coppernic.sdk.utils.core.CpcDefinitions;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,12 +13,40 @@ public class RFID extends CordovaPlugin {
 
 	private Context context;
 	private CallbackContext callbackContext;
+	private static final String AGRIDENT_WEDGE = "fr.coppernic.tools.cpcagridentwedge";
+	private String result = "";
+
+	private BroadcastReceiver agridentReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// Clears keyboard wedge edit text
+			if (intent.getAction().equals(CpcDefinitions.ACTION_AGRIDENT_SUCCESS)) {
+				// Displays data read in the intent edit text
+				String dataRead = intent.getStringExtra(CpcDefinitions.KEY_BARCODE_DATA);
+				context.unregisterReceiver(agridentReceiver);
+				result = dataRead;
+			} else if (intent.getAction().equals(CpcDefinitions.ACTION_AGRIDENT_ERROR)) {
+				// Displays no data read in intent edit text
+				result = "error";
+			} else {
+				result = "error";
+			}
+			callbackContext.success(result);
+		}
+	};
 
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
 
 		context = this.cordova.getActivity().getApplicationContext();
+
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(CpcDefinitions.ACTION_AGRIDENT_SUCCESS);
+		intentFilter.addAction(CpcDefinitions.ACTION_AGRIDENT_ERROR);
+		context.registerReceiver(agridentReceiver, intentFilter);
+
+
 	}
 
 	@Override
@@ -28,6 +60,7 @@ public class RFID extends CordovaPlugin {
 	}
 
 	private void scan() {
-		callbackContext.success("123456789123456");
+		Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(AGRIDENT_WEDGE);
+		context.startActivity(launchIntent);
 	}
 }
